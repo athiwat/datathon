5313842dc19617b7b18dcb76c9e8316ac455f399
Th1nkK1D <witheep@gmail.com>
2020-09-11 21:00:27 +0700

    Add topic card and select before sort mechanic

---
+++ src/components/game/TopicCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    :class="
+      `p-5 text-center rounded shadow border-2 ${
+        !isSorting && topic.isSelected ? 'border-green-400' : ''
+      } ${isSorting ? 'cursor-move' : 'cursor-pointer'}`
+    "
+    @click="onClick"
+  >
+    {{ topic.name }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TopicCard",
+  props: ["topic", "onClick", "isSorting"]
+};
+</script>
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,40 +1,110 @@
 <template>
-  <div>
+  <div class="flex flex-col space-y-4">
     <h1 class="text-3xl">Game</h1>
-
+    <div>
+      <h2 class="text-xl">
+        {{
+          !isSorting
+            ? `เลือกปัญหาที่อยากได้รับหารแก้ไขทั้งหมด 5 อันดับ (เหลืออีก ${numberOfSelectableTopic})`
+            : "เรียงลำดับตามความสำคัญ"
+        }}
+      </h2>
+    </div>
     <draggable
       tag="div"
-      class="grid grid-cols-3 gap-4"
+      class="grid grid-cols-5 gap-4"
       v-model="topics"
       :animation="200"
       ghostClass="opacity-0"
+      :disabled="!isSorting"
     >
-      <div
-        class="p-5 text-center rounded shadow"
-        v-for="topic in topics"
-        :key="topic.name"
-      >
-        {{ topic.name }}
-      </div>
+      <TopicCard
+        v-for="(topic, index) in displayTopics"
+        :key="topic.key"
+        :topic="topic"
+        :onClick="() => selectOption(index)"
+        ฃ
+        :isSorting="isSorting"
+      />
     </draggable>
+
+    <div class="text-right space-x-2">
+      <button
+        v-if="isSorting"
+        class="rounded bg-gray-300 px-4 py-2"
+        @click="isSorting = false"
+      >
+        Back
+      </button>
+      <button
+        :class="
+          `rounded bg-gray-300 px-4 py-2 ${
+            numberOfSelectableTopic !== 0 ? 'opacity-50 cursor-not-allowed' : ''
+          }`
+        "
+        @click="isSorting = true"
+      >
+        Next
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
 import draggable from "vuedraggable";
+import TopicCard from "../components/game/TopicCard";
+
+const MAX_SELECTED_TOPICS = 5;
+const topics = [
+  { key: "transportation", name: "การคมนาคม" },
+  { key: "environment", name: "สิ่งแวดล้อม" },
+  { key: "public_space", name: "พื้นที่สาธารณะ" },
+  { key: "safety", name: "ความปลอดภัย" },
+  { key: "flood", name: "น้ำท่วม" },
+  { key: "inequality", name: "ค่าครองชีพและความเลื่อมล้ำ" },
+  { key: "education", name: "การศึกษา" },
+  { key: "health", name: "ระบบสาธารณะสุข" },
+  { key: "governance", name: "ระบบบริหารราชการ" }
+];
+
 export default {
   name: "Game",
   components: {
-    draggable
+    draggable,
+    TopicCard
   },
   data() {
     return {
-      topics: [
-        { name: "การคมนาคม" },
-        { name: "สิ่งแวดล้อม" },
-        { name: "พื้นที่สาธารณะ" }
-      ]
+      topics: topics.map(topic => ({ ...topic, isSelected: false })),
+      isSorting: false
     };
+  },
+  computed: {
+    selectedTopics() {
+      return this.topics.filter(({ isSelected }) => isSelected);
+    },
+    displayTopics() {
+      return this.isSorting ? this.selectedTopics : this.topics;
+    },
+    numberOfSelectableTopic() {
+      return MAX_SELECTED_TOPICS - this.selectedTopics.length;
+    }
+  },
+  methods: {
+    selectOption(index) {
+      if (this.isSorting) {
+        return;
+      }
+
+      const isSelected = !this.topics[index].isSelected;
+
+      if (!isSelected || this.numberOfSelectableTopic > 0) {
+        this.$set(this.topics, index, {
+          ...this.topics[index],
+          isSelected
+        });
+      }
+    }
   }
 };
 </script>
