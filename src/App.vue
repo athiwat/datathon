d72bc759df766a63635dff1183b0afb42c36e570
Th1nkK1D <witheep@gmail.com>
2020-09-11 16:53:42 +0700

    Clean up unused template and init game view

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
-  <div id="app" class="text-center">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
-    </div>
+  <div id="app">
     <router-view />
   </div>
 </template>
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <h1 class="text-3xl">Datathon Landing page</h1>
+    <h1 class="text-3xl">Game</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Home"
+  name: "Game"
 };
 </script>
