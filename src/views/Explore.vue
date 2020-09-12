23e687b411b44fbce4b0b661bf4741da883487a0
Michael Wongwaisayawan <athiwat@gmail.com>
2020-09-12 15:39:08 +0700

    Adding show/hide mechanism

--- src/views/Explore.vue
+++ src/views/Explore.vue
@@ -1,9 +1,21 @@
 <template>
-  <div>
-    Explore
+  <div class="flex mb-4">
+    <div class="w-3/4">
+      Explore
+      <button v-if="isHidden" @click="isHidden = false">reveal</button>
+    </div>
+    <div class="w-1/4">
+      <h1 v-if="!isHidden">Show</h1>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      isHidden: true
+    };
+  }
+};
 </script>
