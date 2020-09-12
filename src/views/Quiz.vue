4cbacee2ae9d5fa7198b259573749386f2fce620
Michael Wongwaisayawan <athiwat@gmail.com>
2020-09-12 11:52:33 +0700

    Adding iFrame - typeform

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
-    <!-- <div class="typeform-widget" data-url="https://form.typeform.com/to/zigHAaRf" style="width: 100%; height: 500px;"></div> 
-    <script> (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() </script> 
-    <div style="font-family: Sans-Serif;font-size: 12px;color: #999;opacity: 0.5; padding-top: 5px;"> powered by <a href="https://admin.typeform.com/signup?utm_campaign=zigHAaRf&utm_source=typeform.com-01EBJ6W132A7RWSPB8CWADQYRC-free&utm_medium=typeform&utm_content=typeform-embedded-poweredbytypeform&utm_term=EN" style="color: #999" target="_blank">Typeform</a> </div> -->
+    <iframe
+      id="typeform-full"
+      width="100%"
+      height="100%"
+      frameborder="0"
+      allow="camera; microphone; autoplay; encrypted-media;"
+      src="https://form.typeform.com/to/zigHAaRf"
+    ></iframe>
   </div>
 </template>
 
