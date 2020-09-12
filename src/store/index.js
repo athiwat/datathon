f11e634e8d6c9330b5c65172357744ddd69ad5db
Th1nkK1D <th1nkk1d@bigdibs-MacBook-Pro.local>
2020-09-12 14:27:34 +0700

    Add vuex persistant store with basic data

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,22 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
+import { v4 as uuidv4 } from "uuid";
+
+import { COLLECTION } from "./collection";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    uid: uuidv4(),
+    [COLLECTION.Quiz]: {},
+    [COLLECTION.Game]: {}
+  },
+  mutations: {
+    saveToStore(state, { collection, data }) {
+      Vue.set(state, collection, data);
+    }
+  },
+  plugins: [createPersistedState()]
 });
