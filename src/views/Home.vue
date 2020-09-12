543eea5d591f6bff53477e0462906b5816301cc5
Th1nkK1D <witheep@gmail.com>
2020-09-13 01:42:07 +0700

    Styling homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,36 @@
 <template>
-  <div>
-    <h1 class="text-3xl">Datathon Landing page</h1>
-    <div />
-    <router-link to="/quiz">Quiz</router-link>
-    <div />
-    <router-link to="/game">Game</router-link>
-    <div />
-    <router-link to="/explore">Explore</router-link>
-    <div />
-
-    <router-link to="/yourvoice">Your voice</router-link>
-    
-    <button class="rounded px-4 py-2 bg-blue-600 text-white" @click="start">
-      Start (init user id)
-    </button>
+  <div id="home" class="w-screen h-screen flex">
+    <div class="m-auto text-center pb-24">
+      <h1 class="font-bold" style="font-size: 15vh">
+        กรุงเทพ
+        <br />ในฝัน
+      </h1>
+      <h2 class="text-2xl">ตรวจสอบ-เข้าใจ-ออกความเห็น ไปด้วยกัน</h2>
+      <div class="mt-12">
+        <router-link
+          to="/quiz"
+          class="rounded-full px-6 py-2 text-2xl"
+          style="background-color:#FECC2F; box-shadow: 0px 4px 10px #FECC2F;"
+          >เริ่มเล่น</router-link
+        >
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapActions } from "vuex";
-import { COLLECTION } from "../storage/collection";
-
 export default {
   name: "Home",
-  methods: {
-    ...mapActions(["saveCollectionToFirebase"]),
-    start() {
-      this.saveCollectionToFirebase({
-        collection: COLLECTION.User
-      });
-      // go to next page
-    }
-  }
+  methods: {}
 };
 </script>
+
+<style lang="css" scoped>
+#home {
+  background: url(../assets/bg.png) no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+</style>
