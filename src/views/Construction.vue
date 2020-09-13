beb067023b37e89ecf15a36473489ef9440994a8
Michael Wongwaisayawan <athiwat@gmail.com>
2020-09-13 21:35:19 +0700

    Adding your voice link in construction

--- src/views/Construction.vue
+++ src/views/Construction.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="text-center">
     <img src="../assets/construction.png" />
-    <router-link to="/">หน้าหลัก</router-link>
+    <div class="text-center">
+      <router-link
+        to="/yourvoice"
+        class="block rounded bg-gray-300 px-4 py-2 w-64 m-auto"
+      >
+        เสียงของคุณ
+      </router-link>
+    </div>
   </div>
 </template>
 
