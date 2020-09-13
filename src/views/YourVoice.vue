ad2d1dc6908010585e30d9f5199d7f9e670a75bd
Michael Wongwaisayawan <athiwat@gmail.com>
2020-09-13 11:36:04 +0700

    Adding typeform to your voice

--- src/views/YourVoice.vue
+++ src/views/YourVoice.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
-    Your voice
-    <router-link to="/">Home</router-link>
+    <iframe
+      id="typeform-full"
+      width="100%"
+      height="800px"
+      frameborder="0"
+      allow="camera; microphone; autoplay; encrypted-media;"
+      src="https://form.typeform.com/to/zigHAaRf"
+    ></iframe>
+    <!-- <router-link to="/">Home</router-link> -->
   </div>
 </template>
 
